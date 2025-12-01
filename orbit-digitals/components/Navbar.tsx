db7f51efd1d7957a9a9a1969c0a1522702fb0b9e
Iamdivy69 <pathakdivya016@gmail.com>
2025-12-01 15:27:53 +0530

    Update Navbar.tsx

--- orbit-digitals/components/Navbar.tsx
+++ orbit-digitals/components/Navbar.tsx
@@ -59,7 +59,7 @@ export default function Navbar() {
             <Link href="/" className="flex items-center gap-2 cursor-pointer">
                 <div className="relative w-10 h-10"> 
                 <Image 
-                    src="/logo.png" 
+                    src="/LOGO.png" 
                     alt="Orbit Logo" 
                     fill 
                     className="object-contain"
